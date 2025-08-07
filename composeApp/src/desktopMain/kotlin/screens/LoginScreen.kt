8f65e4ce82ce754f7497a87178b7e93403496b5f
AIGLEZMA <aiglezdev@gmail.com>
2025-08-07 19:20:13 +0100

    fix: Added <ENTER> behaviour for logging in

--- composeApp/src/desktopMain/kotlin/screens/LoginScreen.kt
+++ composeApp/src/desktopMain/kotlin/screens/LoginScreen.kt
@@ -10,12 +10,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.nativeKeyCode
-import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -38,93 +38,119 @@ class LoginScreen : Screen {
         var password by remember { mutableStateOf("") }
         var passwordVisible by remember { mutableStateOf(false) }
 
-        // FocusRequesters for each field and button
         val usernameFocusRequester = remember { FocusRequester() }
         val passwordFocusRequester = remember { FocusRequester() }
         val loginButtonFocusRequester = remember { FocusRequester() }
+
+        var usernameIsFocused by remember { mutableStateOf(false) }
+        var passwordIsFocused by remember { mutableStateOf(false) }
+        var loginButtonIsFocused by remember { mutableStateOf(false) }
+
+        LaunchedEffect(Unit) {
+            usernameFocusRequester.requestFocus()
+        }
 
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .onKeyEvent { keyEvent ->
+                        // We only handle the Enter key here. Tab will be handled by the system's focus traversal.
+                        if (keyEvent.type == KeyEventType.KeyUp) {
+                            when (keyEvent.key) {
+                                Key.Enter -> {
+                                    if (passwordIsFocused || loginButtonIsFocused) {
+                                        screenModel.login(username, password)
+                                        return@onKeyEvent true
+                                    }
+                                }
+                            }
+                        }
+                        false
+                    },
+                contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = "Magrinov",
-                    style = MaterialTheme.typography.headlineLarge
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text("Connectez vous pour accéder à l'application.")
-
-                Spacer(modifier = Modifier.height(16.dp))
-                OutlinedTextField(
-                    value = username,
-                    onValueChange = { username = it },
-                    label = { Text("Nom d'utilisateur") },
-                    singleLine = true,
-                    modifier = Modifier
-                        .fillMaxWidth(0.21f)
-                        .focusRequester(usernameFocusRequester)
-                        .focusProperties { next = passwordFocusRequester }
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                OutlinedTextField(
-                    value = password,
-                    onValueChange = {
-                        password = it.filterNot { char -> char in listOf('\t', '\n') }
-                    },
-                    label = { Text("Mot de passe") },
-                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    trailingIcon = {
-                        val image = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
-                        val description = if (passwordVisible) "Hide password" else "Show password"
-                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
-                            Icon(imageVector = image, contentDescription = description)
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth(0.21f)
-                        .focusRequester(passwordFocusRequester)
-                        .focusProperties { next = loginButtonFocusRequester }
-                        .onKeyEvent { keyEvent ->
-                            if (keyEvent.key.nativeKeyCode == 13) { // 13 is the native key code for ENTER
-                                screenModel.login(username, password)
-                                true
-                            } else {
-                                false
-                            }
-                        }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    onClick = { screenModel.login(username, password) },
-                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-                    elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 4.dp),
-                    modifier = Modifier
-                        .fillMaxWidth(0.21f)
-                        .focusRequester(loginButtonFocusRequester)
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(text = "Connexion", color = Color.White)
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                if (loginState.isLoading) {
-                    CircularProgressIndicator()
+                    Text(
+                        text = "Magrinov",
+                        style = MaterialTheme.typography.headlineLarge
+                    )
                     Spacer(modifier = Modifier.height(8.dp))
-                    Text("Connexion en cours...", color = MaterialTheme.colorScheme.primary)
-                } else {
-                    when {
-                        loginState.isAuthenticated -> {
-                            LaunchedEffect(Unit) {
-                                navigator.push(ReportsScreen())
-                            }
-                        }
+                    Text("Connectez vous pour accéder à l'application.")
 
-                        loginState.errorMessage != null -> {
-                            Text(loginState.errorMessage, color = MaterialTheme.colorScheme.error)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedTextField(
+                        value = username,
+                        onValueChange = { username = it },
+                        label = { Text("Nom d'utilisateur") },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth(0.21f)
+                            .focusRequester(usernameFocusRequester)
+                            .onFocusChanged { focusState -> usernameIsFocused = focusState.isFocused }
+                            .focusProperties {
+                                next = passwordFocusRequester
+                            }
+                    )
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = {
+                            password = it.filterNot { char -> char in listOf('\t', '\n') }
+                        },
+                        label = { Text("Mot de passe") },
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        trailingIcon = {
+                            val image = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
+                            val description = if (passwordVisible) "Hide password" else "Show password"
+                            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                                Icon(imageVector = image, contentDescription = description)
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(0.21f)
+                            .focusRequester(passwordFocusRequester)
+                            .onFocusChanged { focusState -> passwordIsFocused = focusState.isFocused }
+                            .focusProperties {
+                                previous = usernameFocusRequester
+                                next = loginButtonFocusRequester
+                            }
+                    )
+                    Button(
+                        onClick = { screenModel.login(username, password) },
+                        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 4.dp),
+                        modifier = Modifier
+                            .fillMaxWidth(0.21f)
+                            .focusRequester(loginButtonFocusRequester)
+                            .onFocusChanged { focusState -> loginButtonIsFocused = focusState.isFocused }
+                            .focusProperties {
+                                previous = passwordFocusRequester
+                            }
+                    ) {
+                        Text(text = "Connexion", color = Color.White)
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    if (loginState.isLoading) {
+                        CircularProgressIndicator()
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text("Connexion en cours...", color = MaterialTheme.colorScheme.primary)
+                    } else {
+                        when {
+                            loginState.isAuthenticated -> {
+                                LaunchedEffect(Unit) {
+                                    navigator.push(ReportsScreen())
+                                }
+                            }
+                            loginState.errorMessage != null -> {
+                                Text(loginState.errorMessage, color = MaterialTheme.colorScheme.error)
+                            }
                         }
                     }
                 }
